62dc2013f406eb9dc47fb01f82e280807f42a842
Ayush Singh <asayushsingh80@gmail.com>
2024-08-17 02:40:57 +0530

    feat: error checks on input url

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"time"
 
+	"github.com/Ayush-Singh24/chisai-be/helpers"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -21,5 +22,26 @@ type response struct {
 }
 
 func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Could not parse JSON",
+		})
+	}
+
+	// if !govalidator.IsURL(body.URL){
+	// 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+	// 		"error" : "Invalid URL",
+	// 	})
+	// }
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "DOMAIN ERROR",
+		})
+	}
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 	return nil
 }
